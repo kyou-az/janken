bda85ac465cbb443ee7bdff84641ef68cd9654b7
上村一平 <ippeikamimura@gmail.com>
2021-05-15 22:04:32 +0900

    add logic

---
+++ example.js
@@ -0,0 +1,75 @@
+const GOO = 0;
+const PA = 1;
+const CHOKI = 2;
+
+// Math.random()関数は、 0 以上 1 未満 (0 は含むが、 1 は含まない) の範囲で浮動小数点の擬似乱数を返します。
+console.log(Math.random());
+// 乱数に3を掛けて、小数点以下を切り捨てることで、0から2の乱数を生成している。
+console.log(Math.floor(Math.random() * 3));
+
+$("#test").click(function () {
+  startJanken()
+});
+
+
+
+function startJanken() {
+  // 相手の手を乱数で確定する処理
+  let compHand = Math.floor(Math.random() * 3);
+  let myHand = $("#my-hand").val();
+
+  myHand = Number(myHand)
+
+  console.log('自分:' + handToString(myHand) + ' 相手:' + handToString(compHand))
+  //　自分のパターン
+  if (myHand === GOO) {
+
+    // 相手の手のパターン
+    if (compHand === GOO) {
+      console.log('あいこ')
+    } else if (compHand === CHOKI) {
+      console.log('かち')
+    } else if (compHand === PA) {
+      console.log('まけ')
+    }
+
+  } else if (myHand === CHOKI) {
+
+    // 相手の手のパターン
+    if (compHand === GOO) {
+      console.log('まけ')
+    } else if (compHand === CHOKI) {
+      console.log('あいこ')
+    } else if (compHand === PA) {
+      console.log('かち')
+    }
+
+  } else if (myHand === PA) {
+
+    // 相手の手のパターン
+    if (compHand === GOO) {
+      console.log('かち')
+    } else if (compHand === CHOKI) {
+      console.log('まけ')
+    } else if (compHand === PA) {
+      console.log('あいこ')
+    }
+
+  }
+}
+
+function handToString(hand) {
+  switch (hand) {
+    case GOO:
+      return 'グー';
+    case PA:
+      return 'パー';
+    case CHOKI:
+      return 'チョキ';
+  }
+}
+
+
+
+
+
--- study.js
+++ study.js
@@ -19,6 +19,8 @@ value = "aaaaaaa"; // 文字列は「''」か「""」か「``」で囲む
 let a = 10;
 let b = 99;
 
+// letで宣言した変数は書き換えることが可能です。
+a = 3000;
 
 // 足し算
 let add = a + b;
@@ -41,6 +43,72 @@ console.log("掛け算" + mul);
 console.log("割り算" + div);
 console.log("余り" + left);
 
+// 真偽値はYesかNoを表す
+console.log(true);
+console.log(false);
+
+// 定数を表すconst
+const myName = 'Ippei Kamimura';
+console.log(myName);
+
 /**
- * if文
+ * [[if文]]
+ * 
+ * 条件分岐をするために用いる文法です
  */
+
+let myAge = 40;
+
+if (myAge >= 20) {
+  console.log('お酒は飲めます!');
+} else {
+  console.log('お酒は飲めません...');
+}
+
+
+if (myAge <= 10) {
+  console.log('子供');
+} else if (myAge <= 20) {
+  console.log('若い');
+} else if (myAge <= 30) {
+  console.log('おっさん');
+} else {
+  console.log('老害');
+}
+
+/**
+ * [[繰り返しの表現 for文]]
+ */
+
+// 配列という変数で、値を複数保持する変数（arrayとも言う）
+const fruits = ['りんご', 'みかん', 'マンゴー', 'パイナップル'];
+
+// [2]の中の数字のことをインデックスと言う
+// インデックスは0から始まる。
+// console.log(fruits[0]);
+
+let f = 0;
+
+// f = f + 1;
+// f = f + 1;
+// f = f + 1;
+// f = f + 1;
+// f = f + 1;
+// f = f + 1;
+
+f++
+f++
+f++
+f++
+f++
+
+console.log(fruits.length);
+
+for (let i = 0; i < fruits.length; i++) {
+  console.log(fruits[i] + ':' + i);
+}
+
+
+
+
+
