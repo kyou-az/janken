2a41e11dc374c4363bc884453edc4797a782f412
kyousuke ohta <suzumi@kyousukenoMacBook-Pro.local>
2021-05-15 20:45:11 +0900

    aaaaaaaaa

---
+++ study.js
@@ -0,0 +1,45 @@
+/**
+ * [[変数]]
+ *
+ * [変数の種類]
+ * string => 文字列
+ * int|number|integer => 数値（整数）
+ * float|double => 小数点
+ * boolean|bool => 真偽値(true or false)
+ */
+
+let value; // 書き換えることができる
+
+value = "aaaaaaa"; // 文字列は「''」か「""」か「``」で囲む
+
+// 文字列のことをプログラミングの世界では「string」と呼びます
+
+// 数値型（numberとかint）は計算ができる
+
+let a = 10;
+let b = 99;
+
+// 足し算
+let add = a + b;
+
+// 引き算
+let sub = a - b;
+
+// 掛け算
+let mul = a * b;
+
+// 割り算
+let div = a / b;
+
+// 余りを算出
+let left = b % a;
+
+console.log("足し算" + add);
+console.log("引き算" + sub);
+console.log("掛け算" + mul);
+console.log("割り算" + div);
+console.log("余り" + left);
+
+/**
+ * if文
+ */
